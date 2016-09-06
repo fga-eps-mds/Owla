0d4ebfc66d88b5f776b3af512b0ed91f47810f94
Álax Alves <alaxallves@gmail.com>
2016-09-05 21:55:26 -0300

    Adding valid member test

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -1,6 +1,13 @@
 require 'test_helper'
 
 class MemberTest < ActiveSupport::TestCase
+	def setup 
+		@member = Member.new(name: "Newest member", email: "testing@email.com", password: "0123456", alias: "nickname")
+	end
+
+	test "tem que rodar" do 
+		assert @member.valid?
+	end
   # test "the truth" do
   #   assert true
   # end
