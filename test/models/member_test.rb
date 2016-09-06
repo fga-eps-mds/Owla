480f0802c71228701d6b840a3b9b33078d443b7d
Álax Alves <alaxallves@gmail.com>
2016-09-05 22:28:17 -0300

    validating email and password presence

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -13,6 +13,14 @@ class MemberTest < ActiveSupport::TestCase
 		@member.name = ""
 		assert_not @member.valid?
 	end 
+	test "deve conter o email" do
+		@member.email = ""
+		assert_not @member.valid?
+	end
+	test "deve conter a senha" do
+		@member.password = ""
+		assert_not @member.valid?
+	end
   # test "the truth" do
   #   assert true
   # end
