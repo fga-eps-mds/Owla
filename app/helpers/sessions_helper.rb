1573d8682f489efbf582122074c882f8ed4d72f1
MatheusRich <matheusrichardt@gmail.com>
2016-09-08 00:21:34 -0300

    Adding log out

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,19 @@
 module SessionsHelper
 
   def log_in(member)
-    session[:member_id] = member.id    
+    session[:member_id] = member.id
+  end
+
+  def current_member
+    @current_member ||= Member.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def log_out
+    session.delete(:member_id)
+    @current_member = nil
   end
 end
