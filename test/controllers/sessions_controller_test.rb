cd52a09485b375a816d83d0edf7b744c4a83962c
Lucas <lucas465@hotmail.com>
2016-09-09 02:10:24 -0300

    Testing sessions controller

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -6,4 +6,14 @@ class SessionsControllerTest < ActionDispatch::IntegrationTest
     get sessions_new_url
     assert_response :success
   end
+
+  test "should create session" do
+  	post login_path, params{session:{email:"member@member" , password:"password123"}}
+  	assert_redirect_to member_path
+  end
+
+  test "should destroy session" do
+  	delete login_path
+  	assert_redirect_to root_url #ADICIONAR DEPOIS A HOMEPAGE
+  end
 end
