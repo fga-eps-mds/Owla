b7ab6401dd0c63aaf54fd17086f67ecefc6a152c
Matheus Joranhezon <mjoranhezon@gmail.com>
2016-09-10 16:59:10 -0300

    Creating relationship between member and room

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,4 +4,6 @@ class Room < ApplicationRecord
   has_many :members, through: :memberships
   validates :name, presence: true, length: {maximum:255, minimum:2}
   
+  validates :name, presence: true, length: {maximum:255, minimum:2}
+
 end
