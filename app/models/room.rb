b2a48bbef9477a773a724b8d032c1729b46e1fa4
MatheusRich <matheusrichardt@gmail.com>
2016-09-13 13:25:29 -0300

    Creating association between Member and Room.

--- app/models/room.rb
+++ app/models/room.rb
@@ -5,5 +5,7 @@ class Room < ApplicationRecord
   validates :name, presence: true, length: {maximum:255, minimum:2}
   
   validates :name, presence: true, length: {maximum:255, minimum:2}
-
+  has_many_and_belongs_to_many :members
+  #has_many :topics
+  
 end
