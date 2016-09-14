2d460c14b5a9e17639d6693e07575438bad60c94
Matheus Bernardo <matheussbernardo@gmail.com>
2016-09-13 23:47:46 -0300

    fixing rooms controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   resources :members
+
   get 'sessions/new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
+
+  resources :rooms
+
 end
