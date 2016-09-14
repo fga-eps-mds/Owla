38aa6c5c61497345f0e6ce00e287bcb38c37a9ee
Matheus Bernardo <matheussbernardo@gmail.com>
2016-09-13 23:47:46 -0300

    fixing rooms controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
 
-  resources :rooms
   #post '/rooms/:id/signup', to: 'rooms#signup'
 
+  resources :rooms
 end
