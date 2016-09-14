f42cf81ab33c55c9c0682f3050ecce3da5d1812b
Lucas <lucas465@hotmail.com>
2016-09-13 22:23:14 -0300

    Correcting answers controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,39 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  def index
-    @answer = Answer.all
-  end
-
-  def new
-    @answer = Answer.new
-  end
-
-  def show
-    @answer = Answer.find(params[:id])
-  end
-
-  def create
-    @answer = Answer.new(answer_params)
-    if @answer.save
-      redirect_to answers_path
-    else
-      flash[:alert] = "Answer not created"
-      render 'new'
-    end
-  end
-
-  def edit
-    @answer = Answer.find(params[:id])
-  end
-
-  def destroy
-    @answer = Answer.find(params[:id])
-    @answer.destroy
-  end
-
-  private
-    def answer_params
-    params.require(:answer).permit(:content)
-  end
 end
