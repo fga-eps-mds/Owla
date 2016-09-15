533c8ab400033cbc87c2bc026a31bd3de1d15064
ThalissonMelo <tallisonmelo@hotmail.com>
2016-09-15 20:59:20 -0300

    Adding some tests at the controll

--- test/controllers/topics_controller_test.rb
+++ test/controllers/topics_controller_test.rb
@@ -1,14 +1,40 @@
 require 'test_helper'
 
 class TopicsControllerTest < ActionDispatch::IntegrationTest
+
+	test "should get new" do
+    get '/topics/new'
+    assert_response :success
+  end
   
-#	test "should create topic" do
-#    post '/topics', params: {
-#        topic: {
-#          name: "SomeTopic",
-#        }
-#      }
-#    assert_redirected_to '/topics'
-#  end
+  test "should show topic" do
+    topic = Topic.create(name: 'SomeTopic')
+
+    get '/topics/', params: {id: topic.id}
+    assert_response :success
+  end
+
+  test "should not show topic"  do
+    get '/topics/show', params: {id: 12}
+    assert_response :missing
+  end
+
+	#test "should create topic" do
+    #post '/topics', params: {
+     #   topic: {
+      #    name: "SomeTopic",
+       # }
+      #}
+    #assert_redirected_to '/topics'
+  #end
+
+  test "should not create user" do
+    post '/topics', params: {
+        topic: {
+          name: "SomeTopic",
+        }
+      }
+    assert_select '.notice'
+  end
 
 end
