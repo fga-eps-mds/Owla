40b9b54248bf48bc965da6f052a60d01e85064c5
ThalissonMelo <tallisonmelo@hotmail.com>
2016-09-15 20:23:42 -0300

    Making some changes at the controller

---
+++ test/controllers/topics_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class TopicsControllerTest < ActionDispatch::IntegrationTest
+  
+#	test "should create topic" do
+#    post '/topics', params: {
+#        topic: {
+#          name: "SomeTopic",
+#        }
+#      }
+#    assert_redirected_to '/topics'
+#  end
+
+end
