b4e0fe8bb94655dc6326244bf8123b03f77f4866
Álax Alves <alaxallves@gmail.com>
2016-09-15 13:15:13 -0300

    Adding Answer/Question association

--- db/migrate/20160914010443_create_answers.rb
+++ db/migrate/20160914010443_create_answers.rb
@@ -1,9 +1,15 @@
 class CreateAnswers < ActiveRecord::Migration[5.0]
-  def change
-    create_table :answers do |t|
-      t.text :content
+    def change
+      create_table :answers do |t|
+       t.text :content
 
-      t.timestamps
+       t.timestamps
+      end
+
+      create_table :questions do |t|
+       t.belongs_to :answer, index: true
+
+       t.timestamps
+       end	
     end
-  end
 end
