47c5c2918264b9604d736a5add62869de22754e1
Lucas <lucas465@hotmail.com>
2016-09-16 01:31:11 -0300

    Fixing doubled migration

--- db/migrate/20160910181824_create_rooms.rb
+++ db/migrate/20160910181824_create_rooms.rb
@@ -6,10 +6,9 @@ class CreateRooms < ActiveRecord::Migration[5.0]
       t.timestamps
     end
 
-<<<<<<< HEAD
     create_table :topics do |t|
     	t.belongs_to :room, index:true 
-
+      t.string :name
     	t.timestamps
     end 
     
@@ -17,8 +16,5 @@ class CreateRooms < ActiveRecord::Migration[5.0]
       t.belongs_to :rooms, index: true
       t.belongs_to :members, index: true
     end
-=======
-    
->>>>>>> creating many to many relation between member and room
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,44 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160913155520) do
+ActiveRecord::Schema.define(version: 20160914014147) do
+
+  create_table "members", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password"
+    t.string   "alias"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
+    t.index ["email"], name: "index_members_on_email", unique: true
+  end
+
+  create_table "memberships", force: :cascade do |t|
+    t.integer  "room_id"
+    t.integer  "member_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["member_id"], name: "index_memberships_on_member_id"
+    t.index ["room_id"], name: "index_memberships_on_room_id"
+  end
+
+  create_table "rooms", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "rooms_members", force: :cascade do |t|
+    t.integer "rooms_id"
+    t.integer "members_id"
+    t.index ["members_id"], name: "index_rooms_members_on_members_id"
+    t.index ["rooms_id"], name: "index_rooms_members_on_rooms_id"
+  end
 
   create_table "topics", force: :cascade do |t|
-    t.string   "name"
     t.integer  "room_id"
+    t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["room_id"], name: "index_topics_on_room_id"
