5a03d7eaf326e38d17d7d8c1da4b85a9a35d7e02
Luan Guimarães <luan@live.in>
2016-09-18 02:25:21 -0300

    Creating login form based on AdminLTE template

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   root 'sessions#new'
+<<<<<<< HEAD
+=======
+  post '/rooms/signup' => 'rooms#signup'
+>>>>>>> Creating login form based on AdminLTE template
 
   resources :members
 
