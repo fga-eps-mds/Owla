ae4b2bf77505808b9d4b06715a2a0cd3a69961fb
MatheusRich <matheusrichardt@gmail.com>
2016-09-18 23:34:05 -0300

    Adding Room model tests.

--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -1,7 +1,19 @@
 require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "should not save an unamed room" do
+    @room = Room.create(name:"")
+    assert_not @room.save
+  end
+
+  test "name should be smaller than 256 characters" do
+    @room = Room.create(name:"asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf")
+    assert_not @room.save
+  end
+
+  test "should save a valid room" do
+    @valid = Room.create(name: "Cálculo 1")
+    assert @valid.save
+  end
 end
