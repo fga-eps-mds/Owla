7f3c9690876013d21f48d04ff353619f38e60f73
MatheusRich <matheusrichardt@gmail.com>
2016-09-18 23:35:26 -0300

    Adding Question model tests.

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @room = Room.create(name: "c1")
+    @topic = @room.topics.create(name: "limite")
+  end
+
+  test "question should not be blank" do
+    @question = @topic.questions.create(content: "")
+    assert_not @question.save
+  end
+
+  test "name should be bigger than 4 characters" do
+    @question = @topic.questions.create(content: "12345")
+    assert @question.save
+  end
+
+  test "name should not be smaller than 5 characters" do
+    @question = @topic.questions.create(content: "1234")
+    assert_not @question.save
+  end
 end
