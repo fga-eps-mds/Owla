e0bcfc51c8d551bfacaa492c388e7a4ca856316f
MatheusRich <matheusrichardt@gmail.com>
2016-09-19 16:23:42 -0300

    Adding Room model tests.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,6 +4,5 @@ class Topic < ApplicationRecord
   belongs_to :member
   has_many :questions, dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 255, minimum: 2 }, uniqueness: { case_sensitive: false } 
-
+  validates :name, presence: true, length: { maximum: 255, minimum: 2 }, uniqueness: { case_sensitive: false }
 end
