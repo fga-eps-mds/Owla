326f06f6ee9554464b010ffb370732dd0a9f2173
MatheusRich <matheusrichardt@gmail.com>
2016-09-19 20:50:21 -0300

    Adding Member model tests.

--- test/models/member_test.rb
+++ test/models/member_test.rb
@@ -3,89 +3,114 @@ require 'test_helper'
 class MemberTest < ActiveSupport::TestCase
 
   def setup
-    @member = Member.new(name: "Newest member",  alias: "nickname", email: "testing@email.com",
+    @member = Member.create(name: "Newest member",  alias: "nickname", email: "testing@email.com",
     password: "foobar", password_confirmation: "foobar")
   end
 
-  test "should validate member" do
-    assert @member.valid?
+  test "should save valid member" do
+    assert @member.save
   end
 
-  test "name should not be blank" do
-    @member.name = "	"
-    assert_not @member.valid?
+  test "should not save member with a blank name" do
+    @member.name = ""
+    assert_not @member.save
   end
 
-  test "alias should not be blank" do
-    @member.alias = "	"
-    assert_not @member.valid?
+  test "should not save a member with name smaller than 3 characters" do
+    @member.name = "Ma"
+    assert_not @member.save
   end
 
-  test "email should not be blank" do
-    @member.email = "	"
-    assert_not @member.valid?
+  test "should not save a member with name bigger than 255 characters" do
+    @member.name = "a" * 256
+    assert_not @member.save
   end
 
-  test "password should have 6 characters minimum" do
-    @member.password = @member.password_confirmation = " " * 6
-    assert_not @member.valid?
+  test "should save a member with an exacty 3 or 255 characters name" do
+    @member.name = "Ana"
+    assert @member.save
+    @member.name = "A"*255
+    assert @member.save
   end
 
-  test "password should not be smaller than 5 characters" do
-    @member.password = @member.password_confirmation = "a" * 5
-    assert_not @member.valid?
+  test "should not save member with a blank alias" do
+    @member.alias = ""
+    assert_not @member.save
   end
 
-  test "name should not be bigger than 60 characters" do
-    @member.name = "a" * 61
-    assert_not @member.valid?
-  end
-
-  test "email should not have more than 256 characters" do
-    @member.email = "a" * 256 + "@email.com"
-    assert_not @member.valid?
-  end
-
-  test "alias should not be bigger than 40 characters" do
+  test "should not save member with alias bigger than 40 characters" do
     @member.alias = "a" * 41
-    assert_not @member.valid?
+    assert_not @member.save
   end
 
-  test "password should not be bigger than 15 ch" do
+  test "should save a member with an exacty 1 or 40 characters alias" do
+    @member.alias = "A"
+    assert @member.save
+    @member.alias = "A"*40
+    assert @member.save
+  end
+
+  test "should not save member with a blank email" do
+    @member.email = ""
+    assert_not @member.save
+  end
+
+  test "should not save a member with email bigger than 255 characters" do
+    @member.email = "a" * 256 + "@email.com"
+    assert_not @member.save
+  end
+
+  test "should not save a member with a null password" do
+    @member.password = password_confirmation = nil
+    assert_not @member.save
+  end
+
+  test "should not save member with password smaller than 6 characters " do
+    @member.password = @member.password_confirmation = "1" * 5
+    assert_not @member.save
+  end
+
+  test "should not save member with password bigger than 15 characters" do
     @member.password = "0" * 16
-    assert_not @member.valid?
+    assert_not @member.save
   end
 
-  test "email should obey validation format" do
+  test "should save member with an exactly 6 or 15 characters password" do
+    @member.password = @member.password_confirmation = "1" * 6
+    assert @member.save
+    @member.password = @member.password_confirmation = "1" * 15
+    assert @member.save
+  end
+
+  test "should save email with right validation format" do
     valid_addresses = %w[testing@example.com MEMBER@foo.COM A_MEM-BER@foo.bar.org
     first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @member.email = valid_address
-      assert @member.valid?, "#{valid_address.inspect} should be valid"
+      assert @member.save, "#{valid_address.inspect} should be valid"
     end
   end
 
-  test "email should not obey invalid format" do
+  test "should not save member with email in an invalid format" do
     invalid_addresses = %w[testing@example,com member_at_foo.org member.name@example.
     foo@bar_baz.com foo@bar+baz.com]
     invalid_addresses.each do |invalid_address|
       @member.email = invalid_address
-      assert_not @member.valid?, "#{invalid_address.inspect} should be invalid"
+      assert_not @member.save, "#{invalid_address.inspect} should be invalid"
     end
   end
 
-  test "member should not have duplicate emails" do
+  test "should note save member with duplicate emails" do
     duplicate_member = @member.dup
     duplicate_member.email = @member.email.upcase
     @member.save
-    assert_not duplicate_member.valid?
+    assert_not duplicate_member.save
   end
 
-  test "every emails should be saved in downcase" do
+  test "should save emails in downcase" do
     mixed_case_email = "FoO@ExAmPlE.CoM"
     @member.email = mixed_case_email
     @member.save
     assert_equal mixed_case_email.downcase, @member.reload.email
   end
-
 end
