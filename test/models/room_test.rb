fea2dd3c421b33e0960b4d72c932cfd0e9295bde
MatheusRich <matheusrichardt@gmail.com>
2016-09-19 16:23:42 -0300

    Adding Room model tests.

--- test/models/room_test.rb
+++ test/models/room_test.rb
@@ -2,18 +2,54 @@ require 'test_helper'
 
 class RoomTest < ActiveSupport::TestCase
 
-  test "should not save an unamed room" do
-    @room = Room.create(name:"")
-    assert_not @room.save
-  end
-
-  test "name should be smaller than 256 characters" do
-    @room = Room.create(name:"asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdfasdf")
-    assert_not @room.save
+  def setup
+    @room = Room.create(name: "IAL")
   end
 
   test "should save a valid room" do
-    @valid = Room.create(name: "Cálculo 1")
-    assert @valid.save
+    assert @room.save
+  end
+
+  test "should save room with exactly 2 or 255 characters name" do
+    @valid_1 = Room.create(name: "-"*2)
+    assert @valid_1.save
+    @valid_2 = Room.create(name: "-"*255)
+    assert @valid_2.save
+  end
+
+  test "should not save an unamed room" do
+    @invalid = Room.create(name: nil)
+    assert_not @invalid.save
+  end
+
+  test "should not save a room with name smaller than 2 characters" do
+    @invalid = Room.create(name: "C")
+    assert_not @invalid.save
+  end
+
+  test "should not save a room with name bigger than 255 characters" do
+    @invalid = Room.create(name: "-"*256)
+    assert_not @invalid.save
+  end
+
+  test "should create not nil room" do
+    assert_not_nil @room
+  end
+
+  test "should change name when room is edited" do
+    before = @room.name
+    @room.update_attribute(:name,"Cálculo 1")
+    after = @room.name
+    assert_not_equal before,after
+  end
+
+  test "should delete room" do
+      room_id = @room.id
+      count_before = Room.all.count
+      @room.destroy
+      recovered_room = Room.where(:id => room_id)
+      count_after = Room.all.count
+      assert_equal recovered_room.count, 0
+      assert_equal count_before,count_after+1
   end
 end
--- test/models/topic_test.rb
+++ test/models/topic_test.rb
@@ -1,31 +1,31 @@
 require 'test_helper'
 
 class TopicTest < ActiveSupport::TestCase
- 
-def setup 
+
+def setup
 		@topic = Topic.new(name: "RUP")
 	end
 
-	test "name should not be blank" do 
+	test "should not save a Topic with blank name" do
 		@topic.name = "	  "
-		assert_not @topic.valid?
+		assert_not @topic.save
 	end
-	
-	test "name should be smaller than 2 characters" do 
+
+	test "should not save a room with name smaller than 2 characters" do
 		@topic.name = "a"
-		assert_not @topic.valid?
+		assert_not @topic.save
 	end
-	
-	test "name should be bigger than 15 characters" do 
-		@topic.name = "aaaaaaaaaaaaaaaa"
-		assert_not @topic.valid?
+
+	test "should not save a topic with name bigger than 255 characters" do
+		@topic.name = "a"*256
+		assert_not @topic.save
 	end
-	
-	test "topic should not have duplicate names" do 
+
+	test "should not save a duplicated topic" do
 		duplicate_topic = @topic.dup
 		duplicate_topic.name = @topic.name.upcase
 		@topic.save
-		assert_not duplicate_topic.valid?
+		assert_not duplicate_topic.save
 	end
 
 end
