bf103dfe3df0a656a47a3e4b23655376701cd7eb
MatheusRich <matheusrichardt@gmail.com>
2016-09-18 21:19:59 -0300

    Adding Answer model tests.

--- test/models/answer_test.rb
+++ test/models/answer_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class AnswerTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @room = Room.create(name: "c1")
+    @topic = @room.topics.create(name: "limite")
+    @question = @topic.questions.create(content: "quanto é o limite de x?")
+  end
+
+  test "should not save empty answer" do
+    @invalid = @question.answers.create(content: "")
+    assert_not @invalid.save
+  end
+
+  test "should save a valid answer" do
+    @valid = @question.answers.create(content: "This is valid.")
+    assert @valid.save
+  end
+
 end
