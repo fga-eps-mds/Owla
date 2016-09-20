5850f6fb234c0598ca907c43cfaba44086a905b2
Luan Guimarães <luan@live.in>
2016-09-20 04:01:16 -0300

    Creating timeline presentation for Topics @ Room's show page

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -4,6 +4,6 @@ class Topic < ApplicationRecord
   belongs_to :member
   has_many :questions, dependent: :destroy
 
-  validates :name, presence: true, length: { maximum: 15, minimum: 2 }, uniqueness: { case_sensitive: false } 
+  validates :name, presence: true, length: { maximum: 50, minimum: 2 }, uniqueness: { case_sensitive: false }
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,24 +6,31 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Creating Victor Navarro User"
 member = Member.create!(name: 'Victor Navarro', alias: 'vi',
 email: 'victor@gmail.com', password: 'testtest', password_confirmation: 'testtest')
 
+puts "Creating Luan Guimarães User"
 member2 = Member.create!(name: 'Luan Guimarães', alias: 'lu',
 email: 'luan@gmail.com', password: 'testtest', password_confirmation: 'testtest')
 
+puts "Creating Jessica Cristina User"
 member3 = Member.create!(name: 'Jéssica Cristina', alias: 'je',
 email: 'jessica@gmail.com', password: 'testtest', password_confirmation: 'testtest')
 
+puts "Creating Vitor Barbosa User"
 member4 = Member.create!(name: 'Vitor Barbosa', alias: 'vivi',
 email: 'vitor@gmail.com', password: 'testtest', password_confirmation: 'testtest')
 
-room = Room.create!(name: 'Humanidade e Cidadania', description: 'Professora Sônia Albuquerque')
+puts "Creating Humanidades e Cidadania room"
+member2.rooms.create!(name: 'Humanidade e Cidadania', description: 'Professora Sônia Albuquerque')
 
-room2 = Room.create!(name: 'Fundamentos de Sistemas Distribuidos', description: 'Professor Fernando W. Cruz')
+puts "Creating Fundamentos de Sistemas Distribuidos room"
+member2.rooms.create!(name: 'Fundamentos de Sistemas Distribuidos', description: 'Professor Fernando W. Cruz')
 
-room3 = Room.create!(name: 'Palestra Empreendendo sua Ideia', description: 'Palestrante José Joaquim')
+puts "Creating Palestra Empreendendo sua Igreja room"
+member2.rooms.create!(name: 'Palestra Empreendendo sua Ideia', description: 'Palestrante José Joaquim')
 
-member2.rooms << room
-member2.rooms << room2
-member2.save
+top = member2.topics.create(name: 'Salvando o Planeta')
+member2.rooms.first.topics << top
+top.save
