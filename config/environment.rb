826a153395043fc4f6ae8e136731a8f86353cee6
Vitor Barbosa <vitornga15@gmail.com>
2016-10-07 21:20:26 -0300

    Removing unnecessary cucumber config in environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,3 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-Rails.application.configure do
-  config.gem "cucumber-rails", :lib => "cucumber/rails"
-end
