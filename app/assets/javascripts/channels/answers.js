42a8e7f792942a1e33ec3b24eac7b8683f9c44c1
Luan Guimarães <luan@live.in>
2016-10-15 17:25:51 -0300

    Fixing ActionCable logic to Answers and refactoring some topic views

--- app/assets/javascripts/channels/answers.js
+++ app/assets/javascripts/channels/answers.js
@@ -1,13 +1,10 @@
-App.messages = App.cable.subscriptions.create('AnswersChannel', {  
+App.messages = App.cable.subscriptions.create('AnswersChannel', {
   received: function(data) {
-    $(".box-comments").removeClass('hidden')
-    return $('.box-comments').append(this.renderMessage(data));
-  }, 
+    $("#box-question-" + data.question_id).removeClass('hidden');
+    $('#answer_content').val('');
+    return $("#box-question-" + data.question_id).append(this.renderMessage(data));
+  },
   renderMessage: function(data) {
-    return "<div class=\"box-comment\">\
-              <img src=\"" + data.member_avatar_url + "\" class=\"img-circle img-sm\" alt=\"User Image\">\
-              <div class=\"comment-text\">\
-                <span class=\"username\">" + data.member + "<span class=\"text-muted pull-right\">" + data.date + "</span>\
-                </span>" + data.content + "</div></div>"
+    return data.html;
   }
 });
