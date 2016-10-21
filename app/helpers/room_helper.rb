a111f80e1e84b4f254b3f744f6bccb34415206b0
Victor Navarro <victor.matias.navarro@gmail.com>
2016-10-21 16:23:23 -0200

    Creating permission validation for ban, reintegrate and list banned members methods and fixing flash
    Signed-off-by: MatheusRich <matheusrichardt@gmail.com>
    Signed-off-by: Victor Navarro <victor.matias.navarro@gmail.com>

--- app/helpers/room_helper.rb
+++ app/helpers/room_helper.rb
@@ -1,2 +1,12 @@
 module RoomHelper
+
+  def is_owner
+    room = Room.find_by(id: params[:id])
+    member = current_member
+    unless room.owner == member
+      flash[:notice] = "You do not have permission to do this action"
+      redirect_to home_path(member)
+    end
+  end
+
 end
