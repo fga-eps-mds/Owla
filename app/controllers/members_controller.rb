5c576de7c071cdd09d931a70e20dad8404897f2c
Lucas Martins <lucas465@hotmail.com>
2016-11-23 16:18:06 -0200

    Fixing some titles
    Signed-off-by: Lucas Martins <lucas465@hotmail.com>
    Signed-off-by: Vitor Barbosa <vitornga15@gmail.com>

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -59,12 +59,14 @@ class MembersController < ApplicationController
   def joined_rooms
     @rooms = current_member.rooms.all + current_member.my_rooms.all
     @subtitle = 'Joined rooms'
+    @title = 'Joined rooms'
     render 'rooms'
   end
 
   def my_rooms
     @rooms = current_member.my_rooms.all
     @subtitle = 'My own rooms'
+    @title = 'My rooms'
     render 'rooms'
   end
 
