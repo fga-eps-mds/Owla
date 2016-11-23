1caccb95bfd8aec229a79ed4323a722bad7ece10
Matheus Joranhezon <mjoranhezon@gmail.com>
2016-11-23 14:54:51 -0200

    Creating acceptance tests to room

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -2,6 +2,11 @@ Given(/^I go to (.+)$/) do |path|
   visit path
 end
 
+Given(/^I visit "(.+)" edit room/) do |object|
+room = Room.find_by(name: object)
+visit edit_room_path(room)
+end
+
 When(/^I click "(.+)"$/) do |link|
   click_link(link)
 end
@@ -24,13 +29,44 @@ Then(/^I should be in "(.+)" edit$/) do |object|
   assert current_path == "/members/#{member.id}/home"
 end
 
+Then(/^I should be in "(.+)"'s rooms$/) do |object|
+  member = Member.find_by(name: object)
+  assert current_path == "/members/#{member.id}/myrooms"
+end
+
+Then(/^I should be in "(.+)"'s joined rooms$/) do |object|
+  member = Member.find_by(name: object)
+  assert current_path == "/members/#{member.id}/joined"
+end
+
+Then(/^"(.+)" should be in all rooms page$/) do |object|
+  member = Member.find_by(name: object)
+  assert current_path == "/members/#{member.id}/rooms"
+end
+
+Then(/^I should be in "(.+)" room$/) do |object|
+  room = Room.find_by(name: object)
+  assert current_path == "/rooms/#{room.id}"
+end
+
+Then(/^I should be in "(.+)" topic$/) do |object|
+  topic = Topic.find_by(name: object)
+  assert current_path == "/topics/#{topic.id}"
+end
+
 When(/^I write "(.+)" on "(.+)"$/) do |value, field|
   fill_in field, :with => value
 end
 
+
 Given(/^I have created "(.+)"$/) do |value|
   member = Member.create(name: "Victor Navarro", email: value, alias: "vivi",
   password: 'testtest', password_confirmation: 'testtest')
+end
+
+Given(/^"(.+)" has created "(.+)" room$/) do |object, value|
+  member = Member.find_by(name: object)
+  room = Room.create(name: value, description: "2015/1", owner_id: member.id)
 end
 
 And(/^the object with attribute "(.+)" and value "(.+)" of "(.+)" klass was created$/) do |attribute, value, klass|
